388595363f66a68d171533676909e05b9200d595
Domnic Raj D <domnic.raj.d@spglobal.com>
2024-01-03 20:09:41 +0530

    changes by domnic

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "github" {}
+provider "github" {
+  token = "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"
+  owner = "Domnic-Raj"
+}
 
 # Retrieve information about the currently (PAT) authenticated user
 data "github_user" "self" {
-  username = ""
+  username = "ddomnicraj"
 }
