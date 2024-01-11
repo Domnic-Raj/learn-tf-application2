98929330d8b0c462e713c8df8f8635486feafe85
Domnic Raj D <domnic.raj.d@spglobal.com>
2024-01-11 13:13:55 +0530

    modified for new repos

--- repositories1.tf
+++ repositories1.tf
@@ -2,12 +2,12 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # Create infrastructure repository
-resource "github_repository" "infrastructure" {
+resource "github_repository" "infrastructure1" {
   name = "learn-tf-infrastructure1"
 }
 
 # Add memberships for infrastructure repository
-resource "github_team_repository" "infrastructure" {
+resource "github_team_repository" "infrastructure1" {
   for_each = {
     for team in local.repo_teams_files["infrastructure"] :
     team.team_name => {
@@ -22,12 +22,12 @@ resource "github_team_repository" "infrastructure" {
 }
 
 # Create application repository
-resource "github_repository" "application" {
+resource "github_repository" "application1" {
   name = "learn-tf-application1"
 }
 
 # Add memberships for application repository
-resource "github_team_repository" "application" {
+resource "github_team_repository" "application1" {
   for_each = {
     for team in local.repo_teams_files["application"] :
     team.team_name => {
